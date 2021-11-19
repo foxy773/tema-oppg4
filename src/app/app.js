b3e342394a47e42ee4f4e89de84b0b39b7679758
foxy773 <felix_1999@live.no>
2021-11-19 14:50:45 +0100

    :art: VUE initialized

--- src/app/app.js
+++ src/app/app.js
@@ -4,6 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from '.App.vue';
 import routes from '../routes.js'
 
+const router = createRouter({
+    routes: routes,
+    history: createWebHistory()
+})
+
 createApp(App)
     .use(router)
     .mount('#app');
