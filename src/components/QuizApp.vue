a6d9bdc90188fe916b8b3ea9fb768c2a6ecaaa6a
Your Name <you@example.com>
2021-11-21 17:58:42 +0100

    :construction: Working on components

--- src/components/QuizApp.vue
+++ src/components/QuizApp.vue
@@ -1,5 +1,14 @@
 <template>
-	<section>
+	<section class="quizContainer">
+		<div class="quizContainer__Question">
+			<h2>What is the name of the capital in Italy?</h2>
+		</div>
+		<div class="quizContainer__Answers">
+			<button />
+			<button />
+			<button />
+			<button />
+		</div>
 	</section>
 </template>
 
@@ -8,4 +17,18 @@
 
 
 <style>
+.quizContainer {
+	width: 100%;
+	height: 100%;
+	}
+
+.quizContainer__Question {
+	
+	}
+
+.quizContainer__Answers{
+	display: grid;
+	grid-template: 2fr / 2fr;
+	}
+
 </style>
