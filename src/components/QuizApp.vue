e73c9ca6562f507d3ea77d525c93c3961a77e40c
foxy773 <felix_1999@live.no>
2021-12-06 15:37:39 +0100

    :lipstick: Small UI changes and other small fixes

--- src/components/QuizApp.vue
+++ src/components/QuizApp.vue
@@ -1,20 +1,20 @@
 <template>
   <section class="quizContainer">
 
-    <div class="quizContainer__status">
-      <h2>Progress: {{ questionIndex + 1 }}/{{ totalQuestions }}</h2>
+    <div class="quizContainer__status" v-if="!this.resultsTime">
+      <h2>Questions: {{ questionIndex + 1 }}/{{ totalQuestions }}</h2>
     </div>
 
-    <div class="quizContainer__question">
+    <div class="quizContainer__status" v-else>
+      <h2>Your Results</h2>
+    </div>
+
+    <div class="quizContainer__question" v-if="!this.resultsTime">
       <h2>{{ currentQuestion.question }}</h2>
     </div>
 
     <div class="quizContainer__answers" v-if="!this.resultsTime">
-      <button
-        class="answers__options"
-        @click="nextQuestion(answer)"
-        v-for="(answer, index) in currentQuestion.answers"
-        :key="index">
+      <button class="answers__options" @click="nextQuestion(answer)" v-for="(answer, index) in currentQuestion.answers" :key="index">
         {{ answer.answer }}
       </button>
     </div>
@@ -22,9 +22,10 @@
     <div class="quizContainer__results" v-else >
       <div v-for="(answer, index) in answered">
       <img src="./../../assets/wrong.svg" alt="" v-if="answer.wrong">
-      <img src="./../../assets/right.svg" alt="" v-if="!answer.wrong">
+      <img src="./../../assets/right.svg" alt="" v-else>
       <h3>{{ answer.answer }}</h3>
       </div>
+      <button @click="resetQuiz()">Retry</button>
     </div>
   </section>
 
@@ -69,57 +70,57 @@ export default {
           ],
         },
         {
-          question: "How many world seas?",
+          question: "What's the biggest animal in the world?",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "The elephant", wrong: true },
+            { answer: "The blue whale", wrong: false },
+            { answer: "The sperm whale", wrong: true },
+            { answer: "The saltwater crocodile", wrong: true },
           ],
         },
         {
-          question: "Results",
+          question: "Which country is brie cheese originally from?",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "switzerland", wrong: true },
+            { answer: "Italy", wrong: true },
+            { answer: "Norway", wrong: true },
+            { answer: "France", wrong: false },
           ],
         },
         {
-          question: "How many world seas?",
+          question: "What year was Heinz established?",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "1877", wrong: true },
+            { answer: "1869", wrong: false },
+            { answer: "1866", wrong: true },
+            { answer: "1927", wrong: true },
           ],
         },
         {
-          question: "How many world seas?",
+          question: "What is the capital of Iceland?",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "Kópavogsbær", wrong: true },
+            { answer: "Reykjavík", wrong: false },
+            { answer: "Oslo", wrong: true },
+            { answer: "Alajärvi", wrong: true },
           ],
         },
         {
-          question: "How many world seas?",
+          question: "What does IPA stand for",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "International Post Arm", wrong: true },
+            { answer: "Internet Panic Alarm", wrong: true },
+            { answer: "Indian Pale Ale", wrong: true },
+            { answer: "India Pale Ale", wrong: false },
           ],
         },
         {
-          question: "How many world seas?",
+          question: "Who painted the Mona Lisa?",
           answers: [
-            { answer: "September 2nd, 1945", wrong: true },
-            { answer: "September 1st, 1939", wrong: false },
-            { answer: "October 19th, 1937", wrong: true },
-            { answer: "September 2nd 1944", wrong: true },
+            { answer: "Vincent van Gogh", wrong: true },
+            { answer: "Leonardo DiCaprio", wrong: false },
+            { answer: "Leonardo da Vinci", wrong: true },
+            { answer: "Vincent dan Gogh", wrong: true },
           ],
         }
       ],
@@ -136,18 +137,22 @@ export default {
   },
   methods: {
     nextQuestion(answer) {
-      if ((this.questionIndex +3) <= this.questions.length) {
+      if ((this.questionIndex +1) < this.questions.length) {
         this.questionIndex++;
         this.answered.push(answer);
         console.log(this.questions.length, "Total");
         console.log(this.questionIndex, "Question number")
       } else {
-        this.questionIndex++;
         this.answered.push(answer);
         this.resultsTime = true;
         console.log("Done!");
         console.log(this.answered)
       }
+    },
+    resetQuiz() {
+      this.questionIndex = 0;
+      this.answered = [];
+      this.resultsTime = false;
     },
   },
 };
@@ -156,14 +161,16 @@ export default {
 
 <style>
 .quizContainer {
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem;
   background: rgb(41, 41, 41);
   font-family: Arial, Helvetica, sans-serif;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
+  max-height: 350px;
 }
 
 .quizContainer__question {
@@ -172,40 +179,74 @@ export default {
   align-items: center;
   height: 20%;
   color: white;
+  text-align: center;
+  margin: 0 0 1rem 0;
 }
 
 .quizContainer__answers {
   display: grid;
-  height: 25%;
-  width: 33%;
+  height: 40%;
+  width: 80%;
   grid-template: 1fr 1fr/ 1fr 1fr;
   gap: 1rem;
 }
 
 .quizContainer__results {
   display: grid;
-  grid-template: 1fr 1fr/ 1fr 1fr;
+  height: 100%;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(auto-fill, 4rem);
   gap: 1rem;
+  overflow-y: scroll;
 }
 
 .quizContainer__results div{
   display: flex;
   align-items: center;
   background: white;
-  border-radius: 10px;
+  border-radius: 0.6rem;
   padding: 0.6rem;
+  max-height: 4rem;
+}
+
+.quizContainer__results img {
+  padding: 0.5rem;
+}
+
+.quizContainer__results button {
+  padding: 0.5rem;
+  border-radius: 0.6rem;
+  background: crimson;
+  border: 0.2rem solid white;
+  font-weight: bold;
+  font-size: 1.1rem;
+  color: white;
+}
+
+.quizContainer__results button:hover {
+  padding: 0.5rem;
+  border-radius: 0.6rem;
+  background: lightseagreen;
+  border: 0.4rem solid white;
+  font-weight: bold;
+  font-size: 1.2rem;
+  transition: all 0.5s;
 }
 
 .answers__options {
   height: 100%;
   width: 100%;
   border: none;
+  border-radius: 0.6rem;
   cursor: pointer;
   font-weight: bold;
+  font-size: 1rem;
+  padding: 0.5rem;
 }
 
 .quizContainer__status {
   color: white;
+  padding: 0 0 1rem 0;
 }
 
 .answers__options:hover {
