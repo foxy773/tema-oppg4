8aa67c28cc6cc22629e24fcea07a91e5f18e02ef
foxy773 <felix_1999@live.no>
2021-12-06 15:39:37 +0100

    :lipstick: Small UI changes and other small fixes

---
+++ src/views/QuizAppView.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="">
+      <QuizApp />
+   </div>
+</template>
+
+<script>
+   import QuizApp from '../components/QuizApp.vue'
+
+   export default {
+      components: {
+         QuizApp
+      }
+   }
+</script>
+
+<style>
+
+</style>
